8d2ee9104031026ebedb48f7cb221ea5c4b72421
paulamadita <paula.schnatsmeyer@student.htw-berlin.de>
2025-01-02 12:35:53 +0100

    AppLayout and Assets von Danny übernommen

--- src/react/Pages/LandingPage/LandingPage.jsx
+++ src/react/Pages/LandingPage/LandingPage.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-
-import { Container } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 const LandingPage = () => (
-  <Container
-    maxWidth={false}
-    disableGutters
+  <Box
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh', // Höhe des Viewports
+      backgroundColor: '#EA4A4A', // Roter Hintergrund
+      textAlign: 'center'
+    }}
   >
-    Landing
-  </Container>
+    <Typography variant="h3" color="white">
+      Auslastung Notaufnahmen Berlin
+    </Typography>
+  </Box>
 )
 
 export default LandingPage
