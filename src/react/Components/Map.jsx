6b719c0817f1950328b0ecd7c4d489f63720390b
neutralius <Lennart.Kaul@Student.HTW-Berlin.de>
2025-01-02 16:47:23 +0100

    fix for missing standard-icons

--- src/react/Components/Map.jsx
+++ src/react/Components/Map.jsx
@@ -1,5 +1,15 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix für fehlende Standard-Icons
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 const MapWithMarkers = () => {
     const points = [
