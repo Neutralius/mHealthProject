0300dc247d45438929a6e599a9a8788109195af2
egroepper <ellen.groepper@student.htw-berlin.de>
2025-02-01 16:43:08 +0100

    neu: medAlternativesData.js mit den Daten der einzelnen Alternativen

---
+++ src/react/Pages/AlternativesPage/medAlternativesData.js
@@ -0,0 +1,40 @@
+// Quelle der Notdienste: https://www.berlin.de/sen/gesundheit/gesundheitswesen/notdienste/
+
+const medAlternativesData = [
+  {
+    id: 'kassenarzt',
+    name: 'Kassenärztlicher Bereitschaftsdienst',
+    telefon: '116117',
+    website: 'https://www.116117.de/de/index.php',
+    erreichbarkeit: '24/7'
+  },
+  {
+    id: 'kinderarzt',
+    name: 'Kinderärztlicher Bereitschaftsdienst',
+    telefon: '116117',
+    website: 'https://www.kinderaerzte-im-netz.de/adressen/notdienste/',
+    erreichbarkeit: '24/7'
+  },
+  {
+    id: 'zahnarzt',
+    name: 'Zahnärztlicher Notdienst',
+    telefon: '030 89004-333',
+    website: 'https://www.kzv-berlin.de/fuer-patienten/notdienst/zahnaerztlicher-notdienst/nd-zeiten',
+    erreichbarkeit: '24/7'
+  },
+  {
+    id: 'giftnotruf',
+    name: 'Giftnotruf',
+    telefon: '030 19240',
+    website: 'https://giftnotruf.charite.de/',
+    erreichbarkeit: '24/7'
+  },
+  {
+    id: 'apotheken',
+    name: 'Apotheken-Notdienst',
+    website: 'https://www.akberlin.de/patientinnen/notdienstfinder',
+    erreichbarkeit: '24/7'
+  }
+]
+
+export default medAlternativesData
