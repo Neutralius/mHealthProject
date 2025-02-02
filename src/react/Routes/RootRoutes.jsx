216ab272dea24b28b972a2f09a18ccf2e62ff5b1
egroepper <ellen.groepper@student.htw-berlin.de>
2025-02-02 18:32:01 +0100

    AlternativesPage hinzugefügt

--- src/react/Routes/RootRoutes.jsx
+++ src/react/Routes/RootRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import AppLayout from '../Pages/Layout/AppLayout'
+import AlternativesPage from '../Pages/AlternativesPage/AlternativesPage'
 
 const RootRoutes = () => (
   <Router>
     <Routes>
       {/* <Route path="/" element={<LocationModalLayout />} /> */}
       <Route path="/" element={<AppLayout />} />
+      <Route path="/alternatives" element={<AlternativesPage />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   </Router>
