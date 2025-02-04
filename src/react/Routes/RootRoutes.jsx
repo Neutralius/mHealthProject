6447e5f14a5fbc4b646dc1ff75226078cb4fc821
egroepper <ellen.groepper@student.htw-berlin.de>
2025-02-04 18:55:02 +0100

    DarkMode entsprechend in RootRoutes.jsx eingefügt

--- src/react/Routes/RootRoutes.jsx
+++ src/react/Routes/RootRoutes.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import AppLayout from '../Pages/Layout/AppLayout'
 import AlternativesPage from '../Pages/AlternativesPage/AlternativesPage'
 
-const RootRoutes = () => (
+const RootRoutes = ({ darkMode, setDarkMode }) => (
   <Router>
     <Routes>
       {/* <Route path="/" element={<LocationModalLayout />} /> */}
-      <Route path="/" element={<AppLayout />} />
-      <Route path="/alternatives" element={<AlternativesPage />} />
+      <Route path="/" element={<AppLayout darkMode={darkMode} setDarkMode={setDarkMode} />} />
+      <Route path="/alternatives" element={<AlternativesPage darkMode={darkMode} />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   </Router>
 )
+
+RootRoutes.propTypes = {
+  darkMode: PropTypes.bool.isRequired,
+  setDarkMode: PropTypes.func.isRequired
+}
 
 export default RootRoutes
